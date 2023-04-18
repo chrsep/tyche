477bebb3a05af05bb88e9cf06ccbe4080f86b696
Chrisando E. Pramudhita <chrsep@protonmail.com>
2023-04-18 21:18:15 +0700

    add prisma.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
 
   return (
     <div className={"prose p-8"}>
-      <h1 className={"bg-gray-50 text-red-700"} >Home</h1>
+      <h1 className={"bg-gray-50 text-red-700"}>Home</h1>
       <p>{message}</p>
     </div>
   )
